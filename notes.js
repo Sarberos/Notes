8112a3ad73f4efd2b3be1f6dd8515fe2c68b4432
sarberos <denismasters@mail.ru>
2023-11-01 14:35:49 +0300

    Added functionality to button

--- notes.js
+++ notes.js
@@ -39,27 +39,33 @@ renderElement(notes)
     renderElement(notes)
     inputElement.value='' /* clean input bar after btn click */
 }
-    
+ 
+// DELEGATION OF EVENTS 
+listElement.onclick =function (event) {
+    if (event.target.dataset.index) {
+        const index = parseInt(event.target.dataset.index) // 'parseInt' same with 'Number'
+        const type = event.target.dataset.type             // displaus information about 'dataset.type'
+        
+        if(type == 'toggle'){
+            notes[index].completed=!notes[index].completed
+        }
+        else if (type == 'remove') {
+            
+            notes.splice(index,1)                           // delete last element in masiv (which one, how many)
+
+        }
+        renderElement(notes)
+    }  
+}
+// DELEGATION OF EVENTS end
+
 function notes_Template (input,i){      
         return `
         <li class="list_item">
          <span class="${input.completed ? 'text-decoration-line-through' : ' '}" id="list_item_text"> ${input.name } </span>
         <span id="two_button">
             <span id="success">
-                <button class="${input.completed ? 'warning_btn' : 'success_Btn'}" id="success_Btn" data-index="${i}"></button></span>
+                <button class="${input.completed ? 'warning_btn' : 'success_Btn'}" id="success_Btn" data-index="${i}" data-type="toggle"></button></span>
             <span id="cross">
-                <button class="cross_Btn" id ="cross_Btn" data-index="${i}"></button></span>
+                <button class="cross_Btn" id ="cross_Btn" data-index="${i}" data-type="remove"></button></span>
         </span></li>`}
-
-// btnSuccess.onclick= function Success() {
-//     return `
-//         <li class="list_item">
-//          <span class="${input.completed ? 'text-decoration-line-through' : ' '}" id="list_item_text"> ${input.name } </span>
-//         <span id="two_button">
-//             <span id="success">
-//                 <button class="${notes.completed ? 'warning_btn' : 'success_Btn'}" id="success_Btn"></button></span>
-//             <span id="cross">
-//                 <button class="cross_Btn" id ="cross_Btn"></button></span>
-//         </span></li>`
-    
-// }
